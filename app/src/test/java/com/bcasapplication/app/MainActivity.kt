0f8f3daecbb9dfa6b1319e7dfc9e570b965fb5f0
parmarkalpesh <bcastudent2003@gmail.com>
2023-12-29 13:05:25 +0000

    Code Synced by DhiWise

---
+++ app/src/test/java/com/bcasapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.bcasapplication.app
+
+import com.bcasapplication.app.appcomponents.base.BaseActivity
+import com.bcasapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
